3de38143f543517216b9200cb50b10bbb1b03abf
Kubilay Kuplay <kubilaykuplay@gmail.com>
2025-11-19 20:50:39 +0300

    first commit

--- app/tr/blog/[slug]/page.tsx
+++ app/tr/blog/[slug]/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import { BLOG_POSTS } from "../../../lib/blog";
+import { BLOG_POSTS } from "../../../../lib/blog";
 
 export const dynamic = "force-static";
 
