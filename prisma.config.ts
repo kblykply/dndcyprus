99c493a6d36f436e014a6b445e097a718fd13f24
Kubilay Kuplay <kubilaykuplay@gmail.com>
2025-11-20 16:46:39 +0300

    first commit

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,13 +4,9 @@ import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
-  engine: "classic",
   datasource: {
     url: env("DATABASE_URL"),
-    // if you want, you can also add:
+    // Eğer istersen ileride:
     // directUrl: env("DATABASE_URL_UNPOOLED"),
   },
 });
